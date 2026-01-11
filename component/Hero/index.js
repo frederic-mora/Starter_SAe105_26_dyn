010c958c7dc9d5f5c42c53f13c2fd248e092626a
Mora Frederic <frederic.mora@unilim.fr>
2026-01-11 20:44:37 +0100

    update comments

--- component/Hero/index.js
+++ component/Hero/index.js
@@ -1,6 +1,9 @@
+// Importation du composant Navigation pour pouvoir l'intégrer dans le composant Hero si besoin
 import { Navigation } from "../Navigation/index.js";
 
+// Chargement du template HTML du composant Hero
 const templateFile = await fetch("component/Hero/template.html");
+// lecture du contenu du fichier template.html sous forme de chaîne de caractères
 const template = await templateFile.text();
 
 let Hero = {
@@ -11,16 +14,25 @@ let Hero = {
    . paramètre data : un objet décrivant les données du Hero
    > valeur de retour : une chaine correspondant au contenu HTML formaté du template du composant Hero avec les données fournies
 */
-Hero.format = function(data, css){
+Hero.format = function(data, css=""){
     let html = template; 
+    // Ajout de la classe CSS optionnelle. Si aucune valeur n'est fournie, 
+    // css est une chaine vide par défaut donc le tag {{cssClass}} sera juste remplacé par une chaine vide
     html = html.replace("{{cssClass}}", css );
+    // Si une navigation a été ajoutée via Hero.addNavigation, on l'insère dans le template.
+    // Sinon, on insère une chaine vide (donc on efface le tag {{nav}}) car Hero.navHTML a été initialisée à vide
     html =  html.replace("{{nav}}", Hero.navHTML );
+    // remplacement des autres tags (les data du composant Hero)
     html = html.replace("{{title}}", data.title );
     html = html.replace("{{subtitle}}", data.subtitle );
     return html;
 }
 
 /* Hero.addNavigation
+
+    IMPORTANT : cette fonction doit être appelée AVANT Hero.render
+    pour que la navigation soit intégrée dans le Hero
+
    . paramètre data : un objet décrivant un menu de navigation
    . paramètre css : une chaine de caractères correspondant à une (ou plusieurs) classe CSS optionnelle à ajouter au composant
    > aucune valeur de retour   
@@ -33,10 +45,10 @@ Hero.addNavigation = function( data, css="" ){
 /* Hero.render
    . paramètre where : un selecteur CSS pour désigner l'élément où ajouter le Hero
    . paramètre data : un objet décrivant les données du Hero
-    . paramètre css : une chaine de caractères correspondant à une (ou plusieurs) classe CSS optionnelle à ajouter au composant
+   . paramètre css : une chaine de caractères correspondant à une (ou plusieurs) classe CSS optionnelle à ajouter au composant
    > aucune valeur de retour   
 */
-Hero.render = function( where, data, css ){
+Hero.render = function( where, data, css="" ){
     let node = document.querySelector(where);
     node.innerHTML += Hero.format( data, css );
 }
--- component/Navigation/index.js
+++ component/Navigation/index.js
@@ -1,34 +1,32 @@
-// const permet de déclarer une variable en lecture seule. 
-// Une variable déclarée avec const ne peut pas être modifiée ultérieurement.
-// C'est une façon de "protéger" une variable pour éviter qu'elle soit modifiée par erreur.
-
-// fetch permet de lire le fichier à l'emplacement indiqué.
-// cette lecture est une opération qui "prend du temps" (asynchrone).
-// await est donc l'instruction utilisée pour dire que l'on attend que l'opération soit terminée.
+ // Chargement du template HTML du composant Navigation
 const templateFile = await fetch("component/Navigation/template.html");
-
 // templateFile.text() pemret de lire et retourner le contenu du fichier sous forme de chaîne de caractères.
-// c'est aussi une opération asynchrone, donc on utilise aussi await pour attendre la fin de l'opération.
 const template = await templateFile.text();
 
-// A ce stade template contient le contenu du fichier template.html sous forme de chaîne de caractères.
-
+// Chargement du template HTML pour un élément de menu (un <li>) 
+// C'est pour gérer la partie variable du menu de navigation (le nombre d'éléments de menu peut varier)
+// (C'est la même chose que les ingrédients du composant Recipe vu en cours)
 const templateLiFile = await fetch("component/Navigation/templateLi.html");
+// lecture du contenu du fichier templateLi.html sous forme de chaîne de caractères
 const templateLi = await templateLiFile.text();
 
 let Navigation = {}; 
 
 /* Recipe.format
    . paramètre data : un objet décrivant un menu de navigation
+   . paramètre css : une chaine de caractères correspondant à une (ou plusieurs) classe CSS optionnelle à ajouter au composant
    > valeur de retour : une chaine correspondant au contenu HTML formaté du template du composant Navigation avec les données fournies
 */
 Navigation.format = function(data, css=""){
     let html = template;
-
+    // Ajout de la classe CSS optionnelle. Si aucune valeur n'est fournie, 
+    // css est une chaine vide par défaut donc le tag {{cssClass}} sera juste remplacé par une chaine vide
     html = html.replace("{{cssClass}}", css );
 
+    // remplacement du titre du menu
     html = html.replace("{{title}}", data.title );
 
+    // génération des éléments de menu (les <li>) à partir du templateLi et des données fournies
     let menuHTML = "";
     for (let menu of data.menus) {
         let li = templateLi;
@@ -36,6 +34,7 @@ Navigation.format = function(data, css=""){
         li = li.replace("{{link}}", menu.link );
         menuHTML += li;
     }
+    // insertion des éléments de menu générés dans le template principal
     html = html.replace("{{menuItems}}", menuHTML );
     return html;
 }
