f7ee960d15b0c544cc5900e922b0a5622dd3151e
Mora Frederic <frederic.mora@unilim.fr>
2026-01-11 20:12:08 +0100

    update comments

--- data/HeroData.js
+++ data/HeroData.js
@@ -1,3 +1,11 @@
+/*
+    Fichier : data/HeroData.js
+    Rôle : contient des données d'exemple pour le composant Hero
+
+    HeroData est un tableau d'objets.
+    Chaque objet contient les propriétés title et subtitle utilisées pour remplir le template du composant Hero.
+    Donc en l'état actuel, HeroData contient 4 ensembles de données différents pour le composant Hero.
+*/
 let HeroData = [
     {
         title: "Welcome to Our Website",
--- data/NavigationData.js
+++ data/NavigationData.js
@@ -1,3 +1,13 @@
+/*
+    Fichier : data/navigationData.js
+    Rôle : contient des données d'exemple pour le composant Navigation
+    
+    NavigationData est un tableau d'objets.
+    Chaque objet contient les propriétés title et menus utilisées pour remplir le template du composant Navigation.
+    La propriété menus est un tableau d'objets, chaque objet contenant les propriétés name et link utilisées pour remplir les éléments de menu dans le template du composant Navigation.
+    
+    Donc en l'état actuel, NavigationData contient 4 ensembles de données différents pour le composant Navigation.
+*/
 let NavigationData = [
     {
         title: "StartUp",
